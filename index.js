a46ff21e70132a775f43c646caad19c43362a98c
Kathy <kleboeuf@townisp.com>
2018-06-03 00:53:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function getFirstSelector(selector) {
+  document.querySelector(selector);
+  return;
+}
+getFirstSelector('div');
+
+function nestedTarget() {
+  
+  
+}
+
+function increaseRankBy(n) {
+  
+  
+}
+
+function deepestChild() {
+  
+  
+}
